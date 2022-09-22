7b32cc2cede9835cf36d447f95bc39868d5636ab
nguyenvudangkhoa189 <nguyenvudangkhoa189@gmail.com>
2022-09-22 22:13:46 +0700

    update new soltution for question 6

--- if-else/q6.cpp
+++ if-else/q6.cpp
@@ -1,19 +1,66 @@
 #include <iostream>
 using namespace std;
 
-int q6a(int num1, int num2, int num3){
-    if ((num1 < num2) && (num2 < num3)){
+int FindMaxBetweenThreeNumbers(int num1, int num2, int num3) {
+    if ((num1 < num2) && (num2 < num3)) {
         return num3;
     }
-    else if((num1 > num2) && (num2 > num3)){
+    else if ((num1 > num2) && (num2 > num3)) {
         return num1;
     }
-    else{
+    else {
         return num2;
+    }
+
+    /*if (num1 > num2 && num1 > num3)
+        return num1;
+    else
+        if (num2 > num1 && num2 > num3)
+            return num2;
+        else
+            return num3;
+
+    int Max = num1;
+    if (Max < num2)
+        Max = num2;
+    if (Max < num3)
+        Max = num3;
+    return Max;*/
+}
+
+void PrintMaxAndVaribleName(int num1, int num2, int num3)
+{
+    int Max = num1;
+    char varibleName = '1';
+
+    if (Max < num2)
+    {
+        Max = num2;
+        varibleName = '2';
+    }
+
+    if (Max < num3)
+    {
+        Max = num3;
+        varibleName = '3';
+    }
+
+    printf("\nMaximum value between %d, %d, %d is %d", num1, num2, num3, Max);
+    switch (varibleName)
+    {
+    case '1':
+        printf("\n varible name is 'num1'");
+        break;
+    case '2':
+        printf("\n varible name is 'num2'");
+        break;
+    case '3':
+        printf("\n varible name is 'num3'");
+        break;
     }
 }
 
-int main(){
+int main() {
     int num1, num2, num3;
 
     cout << "Please enter the first number: ";
@@ -25,15 +72,7 @@ int main(){
     cout << "Please enter the third number: ";
     cin >> num3;
 
-    if (q6a(num1, num2, num3) == num1){
-        cout << "The maximum is: " << num1 << " and the variable is num1" << endl;
-    }
-    else if (q6a(num1, num2, num3) == num2){
-        cout << "The maximum is: " << num2 << " and the variable is num2" << endl;
-    }
-    else{
-        cout << "The maximum is: " << num3 << " and the variable is num3" << endl;
-    }
+    PrintMaxAndVaribleName(num1, num2, num3);
 
     return 0;
 }
