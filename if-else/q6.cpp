446cdfd89057e4152d98de3b6ec031b57d3a9089
hieu1210 <hieu.n.pham1210@gmail.com>
2022-09-22 23:34:08 +0800

    update new files

--- if-else/q6.cpp
+++ if-else/q6.cpp
@@ -2,60 +2,63 @@
 using namespace std;
 
 int FindMaxBetweenThreeNumbers(int num1, int num2, int num3) {
-    if ((num1 < num2) && (num2 < num3)) {
-        return num3;
-    }
-    else if ((num1 > num2) && (num2 > num3)) {
-        return num1;
-    }
-    else {
-        return num2;
-    }
-
-    /*if (num1 > num2 && num1 > num3)
-        return num1;
-    else
-        if (num2 > num1 && num2 > num3)
-            return num2;
-        else
-            return num3;
-
     int Max = num1;
+
     if (Max < num2)
+    {
         Max = num2;
+    }
+
     if (Max < num3)
+    {
         Max = num3;
-    return Max;*/
+    }
+    return Max;
+}
+
+int FindMinBetweenThreeNumbers(int num1, int num2, int num3) {
+    int Min = num1;
+
+    if (num2 < Min)
+    {
+        Min = num2;
+    }
+
+    if (num3 < Min)
+    {
+        Min = num3;
+    }
+    return Min;
 }
 
 void PrintMaxAndVaribleName(int num1, int num2, int num3)
 {
     int Max = num1;
-    char varibleName = '1';
+    char variableName = '1';
 
     if (Max < num2)
     {
         Max = num2;
-        varibleName = '2';
+        variableName = '2';
     }
 
     if (Max < num3)
     {
         Max = num3;
-        varibleName = '3';
+        variableName = '3';
     }
 
     printf("\nMaximum value between %d, %d, %d is %d", num1, num2, num3, Max);
-    switch (varibleName)
+    switch (variableName)
     {
     case '1':
-        printf("\n varible name is 'num1'");
+        printf("\n variable name is 'num1'");
         break;
     case '2':
-        printf("\n varible name is 'num2'");
+        printf("\n variable name is 'num2'");
         break;
     case '3':
-        printf("\n varible name is 'num3'");
+        printf("\n variable name is 'num3'");
         break;
     }
 }
@@ -72,7 +75,14 @@ int main() {
     cout << "Please enter the third number: ";
     cin >> num3;
 
-    PrintMaxAndVaribleName(num1, num2, num3);
+    int sum = num1+num2+num3;
+    int max = FindMaxBetweenThreeNumbers(num1, num2, num3);
+    int min = FindMinBetweenThreeNumbers(num1, num2, num3);
+
+    cout << "Sum: " << sum << "; Max: " << max << "; Min: " << min << endl;
+
+    int secondLargest = sum-max-min;
+    cout << "The second largest number is: " << secondLargest << endl;
 
     return 0;
 }
