96b35349649fd93c0cf0462ef021f2f93cf0eec6
hieu1210 <hieu.n.pham1210@gmail.com>
2022-09-25 21:12:39 +0800

    Update code to Question 6 part c

--- if-else/q6.cpp
+++ if-else/q6.cpp
@@ -16,6 +16,36 @@ int FindMaxBetweenThreeNumbers(int num1, int num2, int num3) {
     return Max;
 }
 
+int FindMaxBetweenSixNumbers(int num1, int num2, int num3, int num4, int num5, int num6) {
+    int Max = num1;
+
+    if (Max < num2)
+    {
+        Max = num2;
+    }
+
+    if (Max < num3)
+    {
+        Max = num3;
+    }
+
+    if (Max < num4)
+    {
+        Max = num4;
+    }
+
+    if (Max < num5)
+    {
+        Max = num5;
+    }
+
+    if (Max < num6)
+    {
+        Max = num6;
+    }
+    return Max;
+}
+
 int FindMinBetweenThreeNumbers(int num1, int num2, int num3) {
     int Min = num1;
 
@@ -31,7 +61,7 @@ int FindMinBetweenThreeNumbers(int num1, int num2, int num3) {
     return Min;
 }
 
-void PrintMaxAndVaribleName(int num1, int num2, int num3)
+void PrintMaxofThreeAndVariableName(int num1, int num2, int num3)
 {
     int Max = num1;
     char variableName = '1';
@@ -63,8 +93,68 @@ void PrintMaxAndVaribleName(int num1, int num2, int num3)
     }
 }
 
+void PrintMaxofSixAndVariableName(int num1, int num2, int num3, int num4, int num5, int num6)
+{
+    int Max = num1;
+    char variableName = '1';
+
+    if (Max < num2)
+    {
+        Max = num2;
+        variableName = '2';
+    }
+
+    if (Max < num3)
+    {
+        Max = num3;
+        variableName = '3';
+    }
+
+    if (Max < num4)
+    {
+        Max = num4;
+        variableName = '4';
+    }
+
+    if (Max < num5)
+    {
+        Max = num5;
+        variableName = '5';
+    }
+
+    if (Max < num6)
+    {
+        Max = num6;
+        variableName = '6';
+    }
+
+    // printf("\nMaximum value between %d, %d, %d is %d", num1, num2, num3, Max);
+    printf("\nMaximum value is %d", Max);
+    switch (variableName)
+    {
+    case '1':
+        printf(" and variable name is 'num1'");
+        break;
+    case '2':
+        printf(" and variable name is 'num2'");
+        break;
+    case '3':
+        printf(" and variable name is 'num3'");
+        break;
+    case '4':
+        printf(" and variable name is 'num4'");
+        break;
+    case '5':
+        printf(" and variable name is 'num5'");
+        break;
+    case '6':
+        printf(" and variable name is 'num6'");
+        break;
+    }
+}
+
 int main() {
-    int num1, num2, num3;
+    int num1, num2, num3, num4, num5, num6;
 
     cout << "Please enter the first number: ";
     cin >> num1;
@@ -75,14 +165,25 @@ int main() {
     cout << "Please enter the third number: ";
     cin >> num3;
 
-    int sum = num1+num2+num3;
-    int max = FindMaxBetweenThreeNumbers(num1, num2, num3);
-    int min = FindMinBetweenThreeNumbers(num1, num2, num3);
+    cout << "Please enter the fourth number: ";
+    cin >> num4;
 
-    cout << "Sum: " << sum << "; Max: " << max << "; Min: " << min << endl;
+    cout << "Please enter the fifth number: ";
+    cin >> num5;
 
-    int secondLargest = sum-max-min;
-    cout << "The second largest number is: " << secondLargest << endl;
+    cout << "Please enter the sixth number: ";
+    cin >> num6;
+
+    // int sum = num1+num2+num3;
+    // int max = FindMaxBetweenThreeNumbers(num1, num2, num3);
+    // int min = FindMinBetweenThreeNumbers(num1, num2, num3);
+
+    PrintMaxofSixAndVariableName(num1, num2, num3, num4, num5, num6);
+
+    // cout << "Sum: " << sum << "; Max: " << max << "; Min: " << min << endl;
+
+    // int secondLargest = sum-max-min;
+    // cout << "The second largest number is: " << secondLargest << endl;
 
     return 0;
 }
