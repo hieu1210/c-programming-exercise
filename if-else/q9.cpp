ba5a4c3ae908ec6262b3d2255d9ff3fb7f2d2d3a
hieu1210 <hieu.n.pham1210@gmail.com>
2022-09-29 22:07:46 +0800

    Commit final version 1 Question 9

--- if-else/q9.cpp
+++ if-else/q9.cpp
@@ -9,8 +9,8 @@ bool isValidTriangle(int side_a, int side_b, int side_c) {
     return false;
 };
 
-int PerimeterTriangle(int side_a, int side_b, int side_c) {
-    int perimeter = side_a + side_b + side_c;
+float PerimeterTriangle(int side_a, int side_b, int side_c) {
+    float perimeter = side_a + side_b + side_c;
     return perimeter;
 };
 
@@ -20,8 +20,41 @@ float AreaTriangle(int side_a, int side_b, int side_c) {
     return area;
 };
 
+void HeightTriangle(int side_a, int side_b, int side_c) {
+    float area = AreaTriangle(side_a, side_b, side_c);
+    float height_a = 2 * area / side_a;
+    float height_b = 2 * area / side_b;
+    float height_c = 2 * area / side_c;
+
+    cout << "Height of side a: " << height_a << "; Height of side b: " << height_b << "; Height of side c: " << height_c << endl;
+}
+
+bool TypeTriangle(int side_a, int side_b, int side_c){
+    bool equilateral, isosceles, right;
+    if ((side_a == side_b == side_c)){
+        equilateral = true;
+        cout << "It's an equilateral triangle." << endl;
+        return true;
+    }
+    if ((side_a == side_b) || (side_b == side_c) || (side_a == side_c)){
+        isosceles = true;
+        cout << "It's an isosceles triangle." << endl;
+        return true;
+    }
+    if (((side_a*side_a + side_b*side_b) == side_c*side_c) || ((side_c*side_c + side_b*side_b) == side_a*side_a) || ((side_a*side_a + side_c*side_c) == side_b*side_b)){
+        right = true;
+        cout << "It's a right triangle." << endl;
+        return true;
+    }
+    if ((isosceles == true) && (right == true)){
+        cout << "It's an isosceles triangle." << endl;
+        return true;
+    }
+    return false;
+}
+
 int main() {
-    int side_a, side_b, side_c;
+    float side_a, side_b, side_c;
 
     cout << "Please enter three sides of a triangle: ";
     cin >> side_a;
@@ -32,6 +65,11 @@ int main() {
         cout << "Valid Triangle." << endl;
         cout << "Perimeter is " << PerimeterTriangle(side_a, side_b, side_c) << endl;
         cout << "Area is " << AreaTriangle(side_a, side_b, side_c) << endl;
+        HeightTriangle(side_a, side_b, side_c);
+        TypeTriangle(side_a, side_b, side_c);
+        if(!(TypeTriangle(side_a, side_b, side_c))){
+            cout << "It's a scalene triangle." << endl;
+        }
     }
     else {
         cout << "Invalid Triangle." << endl;
