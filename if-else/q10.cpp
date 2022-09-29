5614ce22702a7452a61ad07340e907f7ced9b80c
nguyenvudangkhoa189 <nguyenvudangkhoa189@gmail.com>
2022-09-29 21:50:43 +0700

    code review for q10

--- if-else/q10.cpp
+++ if-else/q10.cpp
@@ -2,19 +2,23 @@
 #include <cmath>
 using namespace std;
 
-void haveSolution(int a, int b, int c, int d, int e, int f){
-    int D = a*e - d*b;
-    int Dx = c*e - f*b;
-    int Dy = a*f - d*c;
+void haveSolution(int a, int b, int c, int d, int e, int f) {
+    int D = a * e - d * b;
+    int Dx = c * e - f * b;
+    int Dy = a * f - d * c;
 
-    if (D != 0){
-        cout << "x: " << Dx / D << endl;
-        cout << "y: " << Dy / D << endl;
+    cout << "D = " << D << endl;
+    cout << "Dx = " << Dx << endl;
+    cout << "Dy = " << Dy << endl;
+
+    if (D != 0) {
+        cout << "x: " << Dx / (D * 1.0) << endl;
+        cout << "y: " << Dy / (D * 1.0) << endl;
     }
-    else if ((Dx != 0) || (Dy != 0)){
+    else if ((Dx != 0) || (Dy != 0)) {
         cout << "The system have no solution." << endl;
     }
-    else{
+    else {
         cout << "The system have infinite solutions." << endl;
     }
 }
@@ -23,13 +27,13 @@ void haveSolution(int a, int b, int c, int d, int e, int f){
 int main() {
     int a, b, c, d, e, f;
 
-	cout << "Please enter the first equation: ";
-	cin >> a; cin >> b; cin >> c;
+    cout << "Please enter the first equation: ";
+    cin >> a; cin >> b; cin >> c;
 
     cout << "Please enter the second equation: ";
-	cin >> d; cin >> e; cin >> f;
+    cin >> d; cin >> e; cin >> f;
 
     haveSolution(a, b, c, d, e, f);
 
-	return 0;
+    return 0;
 }
