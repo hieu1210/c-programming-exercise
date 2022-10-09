55ee90dc19ee5f6594b82a9561e8f78aea8005c1
hieu1210 <hieu.n.pham1210@gmail.com>
2022-10-09 22:59:09 +0800

    Update version 2 loops Q1

--- loops/q1.cpp
+++ loops/q1.cpp
@@ -2,32 +2,179 @@
 #include <cmath>
 using namespace std;
 
-void printNaturalNumbers(int n)
+// Question 1a
+void printNumbers(int n)
 {
     // while
-    cout << "While loop";
+    cout << "While loop" << endl;
     int i = 1;
-    while (i < n + 1)
+    while (i <= n)
     {
-        printf("\ni: %d", i);
+        printf("%d; ", i);
         i++;
     }
 
     // for loop
-    cout << "\n\nFor loop";
-    for (int i = 1; i < n + 1; i++)
+    cout << "\n\nFor loop" << endl;
+    for (int i = 1; i <= n; i++)
     {
-        printf("\ni: %d", i);
+        printf("%d; ", i);
     }
 
     // do-while loop
-    cout << "\n\nDo-while loop";
+    cout << "\n\nDo-while loop" << endl;
     i = 1;
     do
     {
-        printf("\ni: %d", i);
+        printf("%d; ", i);
         i++;
-    } while (i < n + 1);
+    } while (i <= n);
+}
+
+// Question 1b
+void printNumbersReverse(int n)
+{
+    // while
+    cout << "While loop" << endl;
+    int i = n;
+    while (i >= 1)
+    {
+        printf("%d; ", i);
+        i--;
+    }
+
+    // for loop
+    cout << "\n\nFor loop" << endl;
+    for (int i = n; i >= 1; i--)
+    {
+        printf("%d; ", i);
+    }
+
+    // do-while loop
+    cout << "\n\nDo-while loop" << endl;
+    i = n;
+    do
+    {
+        printf("%d; ", i);
+        i--;
+    } while (i >= 1);
+}
+
+// Question 1c
+void printOddNumbers(int n)
+{
+    // while
+    cout << "While loop" << endl;
+    int i = 1;
+    while (i <= n)
+    {
+        if (i % 2 == 1){
+            printf("%d; ", i);
+        }
+        i++;
+    }
+
+    // for loop
+    cout << "\n\nFor loop" << endl;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 == 1){
+            printf("%d; ", i);
+        }
+    }
+
+    // do-while loop
+    cout << "\n\nDo-while loop" << endl;
+    i = 1;
+    do
+    {
+        if (i % 2 == 1){
+            printf("%d; ", i);
+        }
+        i++;
+    } while (i <= n);
+}
+
+// Question 1d
+void printSumNumbers(int n)
+{
+    // while
+    cout << "While loop" << endl;
+    int sum = 0;
+    int i = 1;
+    while (i <= n)
+    {
+        printf("%d; ", i);
+        sum += i;
+        i++;
+    }
+    printf("\nSum: %d", sum);
+
+    // for loop
+    cout << "\n\nFor loop" << endl;
+    sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        printf("%d; ", i);
+        sum += i;
+    }
+    printf("\nSum: %d", sum);
+
+    // do-while loop
+    cout << "\n\nDo-while loop" << endl;
+    sum = 0;
+    i = 1;
+    do
+    {
+        printf("%d; ", i);
+        sum += i;
+        i++;
+    } while (i <= n);
+    printf("\nSum: %d", sum);
+}
+
+// Question 1e
+void printSumEvenNumbers(int n)
+{
+    // while
+    cout << "While loop" << endl;
+    int sum = 0;
+    int i = 1;
+    while (i <= n)
+    {
+        if (i % 2 == 0){
+            printf("%d; ", i);
+            sum += i;
+        }
+        i++;
+    }
+    printf("\nSum: %d", sum);
+
+    // for loop
+    cout << "\n\nFor loop" << endl;
+    sum = 0;
+    for (int i = 1; i <= n; i++)
+    {
+        if (i % 2 == 0){
+            printf("%d; ", i);
+            sum += i;
+        }
+    }
+    printf("\nSum: %d", sum);
+
+    // do-while loop
+    cout << "\n\nDo-while loop" << endl;
+    sum = 0;
+    i = 1;
+    do
+    {
+        if (i % 2 == 0){
+            printf("%d; ", i);
+            sum += i;
+        }
+        i++;
+    } while (i <= n);
+    printf("\nSum: %d", sum);
 }
 
 int main()
@@ -40,7 +187,7 @@ int main()
         cin >> n;
     } while (n <= 0);
 
-    printNaturalNumbers(n);
+    printSumEvenNumbers(n);
 
     return 0;
 }
